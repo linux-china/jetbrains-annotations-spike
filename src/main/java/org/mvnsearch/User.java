ca7f442e2ce2628d9cf8cb0903c0ccb9bbfc6024
linux_china <libing.chen@gmail.com>
2020-01-10 14:35:06 -0800

    user class

---
+++ src/main/java/org/mvnsearch/User.java
@@ -0,0 +1,31 @@
+package org.mvnsearch;
+
+import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.Debug;
+
+/**
+ * user class
+ *
+ * @author linux_china
+ */
+@Debug.Renderer(text = "")
+public class User {
+    private Integer id;
+    private String nick;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(@Pattern("[a-zA-Z0-9]+") String nick) {
+        this.nick = nick;
+    }
+}
