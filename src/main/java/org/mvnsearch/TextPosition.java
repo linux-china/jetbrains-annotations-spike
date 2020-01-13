8b05f948707df3d43a41bcac3bac52dac1bacd63
linux_china <libing.chen@gmail.com>
2020-01-13 11:34:48 -0800

    text Position annotation

---
+++ src/main/java/org/mvnsearch/TextPosition.java
@@ -0,0 +1,14 @@
+package org.mvnsearch;
+
+import org.intellij.lang.annotations.MagicConstant;
+
+import static org.mvnsearch.Tag.*;
+
+/**
+ * text Position annotation
+ *
+ * @author linux_china
+ */
+@MagicConstant(intValues = {TOP, CENTER, BOTTOM, LEFT, RIGHT})
+public @interface TextPosition {
+}
