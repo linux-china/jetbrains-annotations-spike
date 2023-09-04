4512e0e495e62a0c706a5f66511ce27abbe92b44
Libing Chen <libing.chen@gmail.com>
2023-09-04 22:23:58 +0800

    test: add @Subst

--- src/test/java/org/mvnsearch/MiscTest.java
+++ src/test/java/org/mvnsearch/MiscTest.java
@@ -1,17 +1,28 @@
 package org.mvnsearch;
 
 
+import org.intellij.lang.annotations.Language;
+import org.intellij.lang.annotations.Subst;
 import org.jetbrains.annotations.Range;
 import org.junit.jupiter.api.Test;
 
 public class MiscTest {
+    @Subst("red")
+    private String color;
+
     @Test
     public void shouldAnswerWithTrue() {
         User vipUser = loadVip(9);
     }
 
     public User loadVip(@Range(from = 1000, to = 2000) Integer id) {
-       return new User();
+        return new User();
+    }
+
+    @Test
+    public void testSubst() {
+        @Language("HTML")
+        String message = "<html><span style='color: " + color + "; font-size:smaller'></span></html>";
     }
 
 }
